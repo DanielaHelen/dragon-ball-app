0e84679766b41f8a234cddb2198d2c101925e0a5
Daniela <danielahgt@ext.inditex.com>
2025-08-21 14:11:18 +0200

    feat: add vitest for testing and update configuration files
    Signed-off-by: Daniela <danielahgt@ext.inditex.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    environmentOptions: {
+      url: "http://localhost",
+    },
+    include: ["**/*.test.js", "**/*.test.ts", "**/*.test.jsx", "**/*.test.tsx"],
+    exclude: ["**/node_modules/**"],
+    setupFiles: ['./config/vitest-setup.ts'],
+  }
 })
