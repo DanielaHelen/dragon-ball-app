f3007cf8b94eae7c370cf6c1590ee8a7f1c88252
Daniela <danielahgt@ext.inditex.com>
2025-08-26 09:57:19 +0200

    refactor(navbar): replace div with Link for logo navigation
    Signed-off-by: Daniela <danielahgt@ext.inditex.com>

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,6 +5,7 @@ import logo from "../../assets/images/Dragon_Ball_Z_logo.svg";
 import { useFavorites } from "../../context/FavoritesContext";
 import ButtonFavorite from "../button-favorite/button-favorite";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const className = "navbar";
@@ -13,12 +14,13 @@ const Navbar = () => {
     const icon = favorites.length > 0 ? { iconImg: iconHeart, iconName: "iconheart" } : { iconImg: hearEmpty, iconName: "iconhearEmpty" };
     return (
         <nav className={className}>
-            <div className={`${className}-logo`} onClick={() => {
-                setShowFavorites(false);
-                navigate("/");
-            }}>
-                <img src={logo} alt='Logo' />
-            </div>
+            <Link
+                className={`${className}-logo`}
+                to="/"
+                onClick={() => setShowFavorites(false)}
+            >
+                <img src={logo} alt='dragon-ball-logo' />
+            </Link>
             <div className={`${className}-favorites`}>
                 <div className={`${className}-icon`}><ButtonFavorite icon={icon} onClick={() => { setShowFavorites(!showFavorites); navigate("/"); }} /></div>
                 <div className={`${className}-text`}>{favorites.length}</div>
