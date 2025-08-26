c8cb57901ec59a3f3f3a17d7a2962ccfa12c1674
Daniela <danielahgt@ext.inditex.com>
2025-08-26 15:29:42 +0200

    fix(vite): add include pattern for coverage in Vite configuration
    Signed-off-by: Daniela <danielahgt@ext.inditex.com>

--- eslint.config.js
+++ eslint.config.js
@@ -37,38 +37,5 @@ export default tseslint.config([
       // Agrega el plugin para React Refresh
       "react-refresh": reactRefresh,
     },
-    rules: {
-      // React
-      // Desactiva la regla que exige importar React en archivos JSX (no es necesario en React 17+)
-      "react/react-in-jsx-scope": "off",
-      // Desactiva la regla que exige dependencias exhaustivas en los hooks de React
-      "react-hooks/exhaustive-deps": "off",
-      // Desactiva la regla que exige que solo se exporten componentes en archivos con React Refresh
-      "react-refresh/only-export-components": "off",
-      // Permite el uso de 'any' en TypeScript (no recomienda su uso)
-
-      // TypeScript
-      "@typescript-eslint/no-explicit-any": "off",
-      // Advierte si hay variables no usadas, pero ignora las que empiezan con '_'
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-      // Advierte sobre expresiones no usadas en TypeScript
-      "@typescript-eslint/no-unused-expressions": "warn",
-
-      // Buenas prácticas
-      // exige === en lugar de ==
-      "eqeqeq": "error",
-      // permitimos console.log en desarrollo
-      "no-console": "off",
-      // alerta si declaras variables sin usar
-      "no-unused-vars": "warn",
-
-      // Estilo (alineado con Prettier)
-      // comillas dobles.
-      "quotes": ["error", "double"],
-      // indentación de 2 espacios
-      "indent": ["error", 2],
-      // usar punto y coma siempre
-      "semi": ["error", "always"],
-    },
   },
 ])
